10fc1a24ded8551089f47b8c203ee2dc9374fb0e
JovanStef <jov.box@outlook.com>
2019-12-27 21:15:53 +0100

    Basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import Person from './Person/Person';
+import UserInput from './userInput/userInput';
+import UserOutput from './userOutput/userOutput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // state = {
+  //   persons: [
+  //     { name: 'Nesto', age: 28 },
+  //     { name: 'Isto', age: 12345 },
+  //     { name: 'Blee', age: 145 }
+  //   ]
+  // }
+  // nameChangeHandler = (newName,newAge) => {
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: newAge },
+  //       { name: 'Isto', age: 12345 },
+  //       { name: 'Blee', age: 145 }
+  //     ]
+  //   })
+  // }
+  // name_ChangeInputHandler = (event) => {
+  //   this.setState({
+  //     persons: [
+  //       { name: "newName", age: "newAge" },
+  //       { name: event.target.value, age: 12345 },
+  //       { name: 'Blee', age: 145 }
+  //     ]
+  //   })
+  // }
+
+  state={
+    name: 'Trpe'
+  }
+  inputChangeName = (event)=>{
+    this.setState({
+      name:event.target.value
+    })
+  }
+  render() {
+    // const buttonStyle ={
+    //   backgroundColor: 'white',
+    //   padding:'5px',
+    //   border:'1px solid blue'
+    // }
+return(
+  <div>
+
+  <UserInput inputChange={this.inputChangeName} userName={this.state.name}/>
+  <UserOutput userName={this.state.name}/>
+  <UserOutput userName={this.state.name}/>
+  <UserOutput userName={this.state.name}/>
+  </div>
+
+)
+  //   return (
+  //     <div className="App">
+  //       <h1>Hi,Nesto</h1>
+  //       <button 
+  //       style={buttonStyle}
+  //       onClick={() =>this.nameChangeHandler( 'CiuCiu!!!',378)}>Name change</button>
+  //       <Person
+  //         name={this.state.persons[0].name}
+  //         age={this.state.persons[0].age}
+  //         click={this.nameChangeHandler.bind(this, 'DrnDRN',256)}
+  //         change={this.name_ChangeInputHandler}
+  //       />
+  //       <Person
+  //         name={this.state.persons[1].name}
+  //         age={this.state.persons[1].age}
+  //         change={this.name_ChangeInputHandler}
+  //         >
+  //         Hobie: Yes please
+  // </Person>
+  //       <Person
+  //         name={this.state.persons[2].name}
+  //         age={this.state.persons[2].age} 
+  //         change={this.name_ChangeInputHandler}
+  //         />
+
+  //     </div>
+  //   );
+  }
 }
 
 export default App;
---
+++ src/userOutput/userOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const userOutput = (props)=>{
+    return(
+    <p>User Name: {props.userName}</p>
+    )
+};
+
+export default userOutput
