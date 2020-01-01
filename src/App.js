131bc150d55a0df89bf15479ca55126f2200b1c0
JovanStef <jov.box@outlook.com>
2020-01-01 20:45:16 +0100

    listing

--- src/App.js
+++ src/App.js
@@ -1,87 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Person from './Person/Person';
-import UserInput from './userInput/userInput';
-import UserOutput from './userOutput/userOutput';
+import Person from './Person/Person';
+// import UserInput from './userInput/userInput';
+// import UserOutput from './userOutput/userOutput';
 
 class App extends Component {
-  // state = {
-  //   persons: [
-  //     { name: 'Nesto', age: 28 },
-  //     { name: 'Isto', age: 12345 },
-  //     { name: 'Blee', age: 145 }
-  //   ]
-  // }
-  // nameChangeHandler = (newName,newAge) => {
-  //   this.setState({
-  //     persons: [
-  //       { name: newName, age: newAge },
-  //       { name: 'Isto', age: 12345 },
-  //       { name: 'Blee', age: 145 }
-  //     ]
-  //   })
-  // }
-  // name_ChangeInputHandler = (event) => {
-  //   this.setState({
-  //     persons: [
-  //       { name: "newName", age: "newAge" },
-  //       { name: event.target.value, age: 12345 },
-  //       { name: 'Blee', age: 145 }
-  //     ]
-  //   })
-  // }
+  state = {
+    persons: [
+      { name: 'Nesto', age: 28 },
+      { name: 'Isto', age: 12345 },
+      { name: 'Blee', age: 145 }
+    ],
+    showPersons:false
 
-  state={
-    name: 'Trpe'
   }
-  inputChangeName = (event)=>{
+  nameChangeHandler = (newName,newAge) => {
     this.setState({
-      name:event.target.value
+      persons: [
+        { name: newName, age: newAge },
+        { name: 'Isto', age: 12345 },
+        { name: 'Blee', age: 145 }
+      ],
     })
   }
+  name_ChangeInputHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "newName", age: "newAge" },
+        { name: event.target.value, age: 12345 },
+        { name: 'Blee', age: 145 }
+      ]
+    })
+  }
+  toggleShow=()=>{
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons:!doesShow})
+  }
+
+
   render() {
-    // const buttonStyle ={
-    //   backgroundColor: 'white',
-    //   padding:'5px',
-    //   border:'1px solid blue'
-    // }
-return(
-  <div>
+    const buttonStyle ={
+      backgroundColor: 'white',
+      padding:'5px',
+      border:'1px solid blue'
+    }
+    let persons=null;
 
-  <UserInput inputChange={this.inputChangeName} userName={this.state.name}/>
-  <UserOutput userName={this.state.name}/>
-  <UserOutput userName={this.state.name}/>
-  <UserOutput userName={this.state.name}/>
-  </div>
+    if(this.state.showPersons){
+      persons=(
+        <div>
+          {this.state.persons.map(person=>{
+            return <Person
+            name={person.name}
+            age={person.age}
+            change={this.name_ChangeInputHandler}
+            />
+          })}
 
-)
-  //   return (
-  //     <div className="App">
-  //       <h1>Hi,Nesto</h1>
-  //       <button 
-  //       style={buttonStyle}
-  //       onClick={() =>this.nameChangeHandler( 'CiuCiu!!!',378)}>Name change</button>
-  //       <Person
-  //         name={this.state.persons[0].name}
-  //         age={this.state.persons[0].age}
-  //         click={this.nameChangeHandler.bind(this, 'DrnDRN',256)}
-  //         change={this.name_ChangeInputHandler}
-  //       />
-  //       <Person
-  //         name={this.state.persons[1].name}
-  //         age={this.state.persons[1].age}
-  //         change={this.name_ChangeInputHandler}
-  //         >
-  //         Hobie: Yes please
-  // </Person>
-  //       <Person
-  //         name={this.state.persons[2].name}
-  //         age={this.state.persons[2].age} 
-  //         change={this.name_ChangeInputHandler}
-  //         />
 
-  //     </div>
-  //   );
+    </div>
+    )
+
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi,Nesto</h1>
+        <button 
+        style={buttonStyle}
+        onClick={this.toggleShow}>Name change</button>
+        {persons}
+      </div>
+    );
   }
 }
 
--- src/userOutput/userOutput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const userOutput = (props)=>{
-    return(
-    <p>User Name: {props.userName}</p>
-    )
-};
-
-export default userOutput
